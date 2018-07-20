18148f54da689f9d6f358879c5eb928cbcfecce2
angelica winkler <caw400@nyu.edu>
2018-07-20 22:01:54 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+katz_deli = []
+
+def line (katz_deli)
+  lineLength = katz_deli.length
+
+  if lineLength == 0
+    puts "The line is currently empty."
+  else
+    lineSummary = []
+    position = 1
+    katz_deli.each do |person|
+      lineSummary.push("#{position}. #{person}")
+      position += 1
+    end
+    puts "The line is currently: #{lineSummary.join(" ")}"
+  end
+
+
+
+end
